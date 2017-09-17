eb4ed940d6945641a82f026b8b4f09ce3ae3ab83
Vincent <752340690@qq.com>
2017-09-17 22:29:58 +0800

    注释语言

---
+++ blog/views.py
@@ -0,0 +1,23 @@
+
+from blog.models import Article
+from datetime import datetime
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    post_list = Article.objects.all()
+    return render(request, 'home.html', {'post_list':post_list})
+
+
+def detail(request, my_args):
+    post = Article.objects.all()[int(my_args)]
+    str = ("title = %s, category = %s, date_time = %s, content = %s"
+           % (post.title, post.category, post.date_time, post.content))
+    return HttpResponse(str)
+
+
+# def test(request):
+#     return render(request, "test.html", {'current_time': datetime.now()})
